32fcc40e52c30ef6f97e403e2a9908c5291f96ad
Kyle Davis <validkeys@gmail.com>
2014-12-04 12:09:48 -0500

    no message

---
+++ api/models/Product.js
@@ -0,0 +1,32 @@
+/**
+* Product.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    title:          {
+      type:     "text",
+      required: true
+    },
+    category:       {
+      type:       "string",
+      enum:       ["computers", "phones", "servers"],
+      required:   true
+    },
+    description:    'sting',
+    price:          {
+      type:         'float',
+      required:     true,
+      defaultsTo:   0.00
+    },
+    quantityOnHand: {
+      type: 'integer',
+      defaultsTo: 0
+    }
+  }
+};
+
